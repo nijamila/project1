0cd55fd07652ec50154a6ae9b8638d2cd4a39f07
nijamila <jamilaxonnumonovaa@gmail.com>
2026-02-12 18:51:07 +0500

    Add support URLs for support and messages views

--- apps/support/urls.py
+++ apps/support/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from .views import support_view, support_thankyou
+
+urlpatterns = [
+    path('', support_view, name='support'),
+    path('thank-you/', support_thankyou, name='support_thankyou'),
+    path('my-messages/', support_messages_view, name='support_messages'),
+]
 
