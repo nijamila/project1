dc41b979a9e926391aaa9421074a6a99766ae265
nijamila <jamilaxonnumonovaa@gmail.com>
2026-02-12 18:48:46 +0500

    Add support views with authentication and message handling

--- apps/support/views.py
+++ apps/support/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import SupportMessageForm
+from .models import SupportMessage
 
-# Create your views here.
+@login_required
+def support_view(request):
+    if request.method == 'POST':
+        form = SupportMessageForm(request.POST)
+        if form.is_valid():
+            support_msg = form.save(commit=False)
+            support_msg.user = request.user
+            support_msg.save()
+            return redirect('support_thankyou')
+    else:
+        form = SupportMessageForm()
+
+    return render(request, 'support/support_form.html', {'form': form})
+
+@login_required
+def support_thankyou(request):
+    return render(request, 'support/support_thankyou.html')
+
+@login_required
+def support_messages_view(request):
+    messages = SupportMessage.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'support/support_messages.html', {'messages': messages})
