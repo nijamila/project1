dd78b1c52f7878709b8eaebae1eb3d895e9cbd9b
nijamila <jamilaxonnumonovaa@gmail.com>
2026-02-12 18:47:13 +0500

    Add SupportMessageForm for support messages

--- apps/support/forms.py
+++ apps/support/forms.py
@@ -1,1 +1,11 @@
+from django import forms
+from .models import SupportMessage
+
+class SupportMessageForm(forms.ModelForm):
+    class Meta:
+        model = SupportMessage
+        fields = ['message']
+        widgets = {
+            'message': forms.Textarea(attrs={'rows':4, 'placeholder':'Type your message here...'})
+        }
 
