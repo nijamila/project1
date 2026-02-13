f07e1ff92140bc4f6813e851bf5dd630bd112162
nijamila <jamilaxonnumonovaa@gmail.com>
2026-02-13 18:14:09 +0500

    Change search field from username to phone

--- apps/support/admin.py
+++ apps/support/admin.py
@@ -5,5 +5,5 @@ from .models import SupportMessage
 class SupportMessageAdmin(admin.ModelAdmin):
     list_display = ('user', 'created_at', 'replied_at', 'message', 'reply')
     readonly_fields = ('user', 'created_at', 'replied_at')
-    search_fields = ('user__username', 'message', 'reply')
+    search_fields = ('user__phone', 'message', 'reply')
     list_filter = ('created_at',)
