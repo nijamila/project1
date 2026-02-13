fd53bdc35ea05de911a11212c970558518abcb64
nijamila <jamilaxonnumonovaa@gmail.com>
2026-02-13 17:35:15 +0500

    Change string representation to use user phone

--- apps/support/models.py
+++ apps/support/models.py
@@ -11,4 +11,4 @@ class SupportMessage(models.Model):
     replied_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
+        return f"{self.user.phone} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
